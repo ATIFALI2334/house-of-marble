2a2bb2713fed0306065a5b45580ebdb9b597e8a0
Muhammad Umair <umairzaheer3268@gmail.com>
2026-09-24 11:47:21 +0500

    Update Facebook URL in site configuration

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -6,7 +6,7 @@ export const site = {
   email: "Houseofmarble786@gmail.com",
   emailAlt: "Houseofmarble92@gmail.com",
   instagram: "https://www.instagram.com/house_ofmarble",
-  facebook: "#", // TODO: replace with your real Facebook page URL
+  facebook: "https://www.facebook.com/share/1AghyW9wy8/", // TODO: replace with your real Facebook page URL
   address: "Plot # VE-226, St-01, Sec-16, Aman Chowk, Khairabad, Orangi Town, Karachi",
 };
 
